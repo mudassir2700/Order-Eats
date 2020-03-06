0b0273b39728910fe31cac1e4d6db674d5504ff6
mudassir2700 <46752405+mudassir2700@users.noreply.github.com>
2020-03-06 16:23:30 +0530

    Update settings.py

--- order_eats/order_eats/settings.py
+++ order_eats/order_eats/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = ['*']
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'anony.chamanee@gmail.com'
-EMAIL_HOST_PASSWORD = 'Pass@1315'
+EMAIL_HOST_USER = 'Your Email'
+EMAIL_HOST_PASSWORD = 'Your Password'
 EMAIL_PORT = 587
 # Application definition
 
